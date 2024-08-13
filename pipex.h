935ec89b5b60c644748e419c1c54276d99bc618d
Andrei Klimchuk <aklimchu@c2r6p11.hive.fi>
2024-08-13 16:02:21 +0300

    practice exercises and beginning of pipex

---
+++ pipex.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aklimchu <aklimchu@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/03 14:31:32 by aklimchu          #+#    #+#             */
+/*   Updated: 2024/08/13 15:30:50 by aklimchu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "./libft/libft.h"
+# include <sys/types.h>
+# include <fcntl.h>
+# include <stdio.h>	//perror
+
+# define BUFFER_SIZE 1024
+
+#endif /* PIPEX_H */
---
+++ practice/fork_practice.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+int	main()
+{
+	pid_t	p = fork();
+	if (p < 0)
+	{
+		perror("fork fail");
+		exit(1);
+	}
+	printf("Hello world!, process_id(pid) = %d\n", getpid());
+	return (0);
+}
