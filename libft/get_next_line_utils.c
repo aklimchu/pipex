febdf562ffd8874b29077ca5db4b9451aea6825f
Andrei Klimchuk <aklimchu@student.hive.fi>
2024-08-27 14:11:18 +0300

    get_next_line function corrected

--- libft/get_next_line_utils.c
+++ libft/get_next_line_utils.c
@@ -6,11 +6,32 @@
 /*   By: aklimchu <aklimchu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 14:07:02 by aklimchu          #+#    #+#             */
-/*   Updated: 2024/08/16 09:37:49 by aklimchu         ###   ########.fr       */
+/*   Updated: 2024/08/27 14:02:55 by aklimchu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+// The function implements the same behaviour as strchr from libc, checking
+// also if input string is a NULL
+
+char	*ft_strchr_new(const char *s, int c)
+{
+	char	new_c;
+
+	if (s == NULL)
+		return ((void *) 0);
+	new_c = c + '\0';
+	while (*s)
+	{
+		if (*s == new_c)
+			return ((char *) s);
+		s++;
+	}
+	if (*s == new_c)
+		return ((char *) s);
+	return ((void *) 0);
+}
 
 // The function implements the same behaviour as strncat from libc.
 
